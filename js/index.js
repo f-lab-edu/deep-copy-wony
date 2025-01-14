104c1f1144cc428da3ea65fd5f9b9d6edff27add
BANGWON LEE <qkddnjs31@gmail.com>
2025-01-14 16:23:15 +0900

    create : deep copy를 테스트 하도록 하는 index 파일

---
+++ js/index.js
@@ -0,0 +1,28 @@
+import { DeepCopyFnc } from './deepCopy.js'
+
+const obj = {
+  num: 123,
+  str: 'abc',
+  arr: ['a', 1, 'b', '2'],
+  obj: {
+    title: 'deepcopy',
+    content: 'create',
+    innerArr: [1, 2, 3, 4, 5],
+    innerObj: {
+      innerTitle: 'innerDeep',
+    },
+  },
+}
+
+const resultCopy = DeepCopyFnc(obj)
+
+resultCopy.obj.title = 'test'
+resultCopy.num = 456
+resultCopy.arr[3] = '333'
+resultCopy.obj.innerArr.push(100)
+resultCopy.obj.innerObj.innerTitle = 'inner 결과'
+
+console.log('original : ', obj)
+console.log('deep copy : ', resultCopy)
+
+console.log('is this real deepCopy no2 : ', obj === resultCopy)
