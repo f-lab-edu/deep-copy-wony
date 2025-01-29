ecaaf76d8fbfe7a6c3d7ba80e073784f4967e560
BANGWON LEE <qkddnjs31@gmail.com>
2025-01-29 13:14:33 +0900

    edit: 피드백에 맞게 코드 수정 진행

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import { DeepCopyFnc } from './js/deepCopy.js'
+import { deepCopyMainFnc } from './js/deepCopy.js'
 
+// const obj = [33]
 const obj = {
   num: 123,
-  str: 'abc',
+  str: 'bc',
   arr: ['a', 1, 'b', '2'],
+  date: new Date(),
   obj: {
     title: 'deepcopy',
     content: 'create',
@@ -14,15 +16,25 @@ const obj = {
   },
 }
 
-const resultCopy = DeepCopyFnc(obj)
+// const num = 3
 
-resultCopy.obj.title = 'test'
-resultCopy.num = 456
-resultCopy.arr[3] = '333'
-resultCopy.obj.innerArr.push(100)
-resultCopy.obj.innerObj.innerTitle = 'inner 결과'
+// console.log('1번', deepCopyMainFnc(3))
+// console.log('resultCoppy :', obj)
 
-console.log('original : ', obj)
-console.log('deep copy : ', resultCopy)
+const resultCopy = deepCopyMainFnc(obj)
 
-console.log('is this real deepCopy no2 : ', obj === resultCopy)
+// resultCopy.obj.title = 'test'
+// resultCopy.num = 456
+// resultCopy.arr[3] = '333'
+// resultCopy.obj.innerArr.push(100)
+// resultCopy.obj.innerObj.innerTitle = 'inner 결과'
+
+// console.log('original : ', obj)
+// const dateV = new Date(resultCopy.date).getTime
+console.log('deep copy : ', dateV)
+
+// console.log('is this real deepCopy no2 : ', obj === resultCopy)
+
+// console.log('ttest', deepCopyMainFnc(obj))
+
+// console.log('coco', copyObj)
--- test/index.test.js
+++ test/index.test.js
@@ -1,35 +1,53 @@
-import { DeepCopyFnc } from '../js/deepCopy'
+import { deepCopyMainFnc } from '../js/deepCopy'
+
+// tobe는 프리미티어 타입
+// toEqual는 객체와 같은 참조 타입
 
 describe('deepcopy check test', () => {
-  const obj = {
-    num: 123,
-    str: 'abc',
-    arr: ['a', 1, 'b', '2'],
-    obj: {
-      title: 'deepcopy',
-      content: 'create',
-      innerArr: [1, 2, 3, 4, 5],
-      innerObj: {
-        innerTitle: 'innerDeep',
-        innerObjArr: [23, 3, 2, 4, 2],
-        dobuleInnerObj: {
-          dobuleInnerTitle: 'wow',
-        },
-      },
-    },
-  }
+  test('원시 타입 복사', () => {
+    const num = 123
+    const copied = deepCopyMainFnc(num)
 
-  const resultCopy = DeepCopyFnc(obj)
+    expect(num).toBe(copied)
+  })
 
-  resultCopy.obj.title = 'test'
-  resultCopy.num = 456
-  resultCopy.arr[3] = '333'
-  resultCopy.obj.innerArr.push(100)
-  resultCopy.obj.innerObj.innerTitle = 'inner 결과'
-  resultCopy.obj.innerObj.dobuleInnerObj.dobuleInnerTitle = 'wow wow wow'
-  resultCopy.obj.innerObj.innerObjArr[4] = 10000
+  test('null값 check', () => {
+    const nullValue = null
+    const copied = deepCopyMainFnc(nullValue)
+  })
 
-  test('obj와 resultcopy는 다르다', () => {
-    expect(obj).not.toEqual(resultCopy)
+  test('객체 복사', () => {
+    const obj = { num: 123, obj: { objNum: 456, str: '가나다' } }
+    const copied = deepCopyMainFnc(obj)
+    copied.obj.str = '라마바'
+
+    expect(copied).not.toEqual(obj)
+    expect(copied.obj.objNum).toBe(obj.obj.objNum)
+    expect(copied.obj.str).not.toBe(obj.obj.str)
+  })
+
+  test('객체 안의 Date 복사', () => {
+    const date = { date: new Date() }
+    const copied = deepCopyMainFnc(date)
+    const editDate = new Date(copied.date).getFullYear(2030)
+
+    expect(editDate).not.toEqual(date)
+    // 수정한 날짜의 값 메모리가 확실히 바뀐 건지 확인하기 위함
+  })
+
+  test('정규식 복사', () => {
+    const reg = { reg: new RegExp(/[^0-9]/) }
+    const copied = deepCopyMainFnc(reg)
+    copied.reg = new RegExp(/[^a-z]/)
+
+    expect(copied.reg).not.toEqual(reg)
+  })
+
+  test('Map 복사', () => {
+    const map = { map: new Map([['name', '이방원']]) }
+    const copied = deepCopyMainFnc(map)
+    copied.map.set('like', 'jejus')
+
+    expect(copied.map).not.toEqual(map)
   })
 })
