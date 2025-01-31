0cbb8f44d3771b3701bf682c29cc96fd1051cc13
BANGWON LEE <qkddnjs31@gmail.com>
2025-01-31 16:11:24 +0900

    edit : 코드 수정

--- js/deepCopy.js
+++ js/deepCopy.js
@@ -1,38 +1,79 @@
 function getNotObjConditon(obj) {
   const notObjCondition =
-    Array.isArray(obj) ||
+    // Array.isArray(obj) ||
     obj === null ||
-    obj instanceof Date ||
-    obj instanceof RegExp ||
-    obj instanceof Map ||
-    obj instanceof Set ||
-    obj instanceof WeakMap ||
-    obj instanceof WeakSet ||
-    typeof obj === 'symbol' ||
+    // obj instanceof Date ||
+    // obj instanceof RegExp ||
+    // obj instanceof Map ||
+    // obj instanceof Set ||
+    // obj instanceof WeakMap ||
+    // obj instanceof WeakSet ||
+    // typeof obj === 'symbol' ||
     typeof obj !== 'object'
 
   return notObjCondition
 }
 
-function getObjectKey(obj) {
-  const objKeyArr = Object.keys(obj)
+// function determineArrType(obj) {
+//   const arrType = Array.isArray(obj) || obj instanceof Set
+//   return arrType
+// }
 
-  return objKeyArr
+function getObjectKey(obj) {
+  console.log('3333', obj)
+  // const target = determineArrType(obj) ? [...obj] : obj
+  const objArr = Object.entries(obj)
+  return objArr
 }
 
 function makeUpObj(objKeyArr, obj) {
   const result = {}
-  objKeyArr.forEach(element => {
-    result[element] = deepCopyMainFnc(obj[element])
+  console.log('objKeARR', objKeyArr)
+
+  objKeyArr.forEach(([key, value], index) => {
+    console.log('objE', key, value)
+    result[key] = deepCopyMainFnc(value)
   })
 
   return result
 }
 
+function deepCopyConstructor(obj) {
+  const result = new obj.constructor([...obj])
+  return result
+}
+
+function deepCopySet(obj) {
+  const result = new Set([...obj])
+  return result
+}
+
+function deepCopyMap(obj) {
+  console.log('map', obj)
+  const result = new Map(obj)
+  return result
+}
+
+// function
+
 export function deepCopyMainFnc(obj) {
   const objTypeStatus = getNotObjConditon(obj)
   if (objTypeStatus) return obj
+  function determineObjFnc() {
+    if (Array.isArray(obj)) {
+      return deepCopyConstructor(obj)
+    } else if (obj instanceof Set) {
+      return deepCopyConstructor(obj)
+    } else if (obj instanceof Map) {
+      return deepCopyConstructor(obj)
+    } else {
+      const objKeysArr = getObjectKey(obj)
+      return makeUpObj(objKeysArr)
+    }
+  }
 
-  const objKeyArr = getObjectKey(obj)
-  return makeUpObj(objKeyArr, obj)
+  return determineObjFnc(obj)
+
+  // const objKeyArr = getObjectKey(obj)
+  // return makeUpObj(objKeyArr, obj)
 }
--- test/index.test.js
+++ test/index.test.js
@@ -4,50 +4,63 @@ import { deepCopyMainFnc } from '../js/deepCopy'
 // toEqual는 객체와 같은 참조 타입
 
 describe('deepcopy check test', () => {
-  test('원시 타입 복사', () => {
-    const num = 123
-    const copied = deepCopyMainFnc(num)
-
-    expect(num).toBe(copied)
-  })
-
-  test('null값 check', () => {
-    const nullValue = null
-    const copied = deepCopyMainFnc(nullValue)
-  })
-
+  // test('원시 타입 복사', () => {
+  //   const num = 123
+  //   const copied = deepCopyMainFnc(num)
+  //   expect(num).toBe(copied)
+  // })
+  // test('null값 check', () => {
+  //   const nullValue = null
+  //   const copied = deepCopyMainFnc(nullValue)
+  // })
   test('객체 복사', () => {
     const obj = { num: 123, obj: { objNum: 456, str: '가나다' } }
     const copied = deepCopyMainFnc(obj)
+    console.log('copied', copied)
     copied.obj.str = '라마바'
-
     expect(copied).not.toEqual(obj)
     expect(copied.obj.objNum).toBe(obj.obj.objNum)
     expect(copied.obj.str).not.toBe(obj.obj.str)
   })
-
-  test('객체 안의 Date 복사', () => {
-    const date = { date: new Date() }
-    const copied = deepCopyMainFnc(date)
-    const editDate = new Date(copied.date).getFullYear(2030)
-
-    expect(editDate).not.toEqual(date)
-    // 수정한 날짜의 값 메모리가 확실히 바뀐 건지 확인하기 위함
-  })
-
-  test('정규식 복사', () => {
-    const reg = { reg: new RegExp(/[^0-9]/) }
-    const copied = deepCopyMainFnc(reg)
-    copied.reg = new RegExp(/[^a-z]/)
-
-    expect(copied.reg).not.toEqual(reg)
-  })
-
+  // test('객체 안의 Date 복사', () => {
+  //   const date = { date: new Date() }
+  //   const copied = deepCopyMainFnc(date)
+  //   const editDate = new Date(copied.date).getFullYear(2030)
+  //   expect(editDate).not.toEqual(date)
+  //   // 수정한 날짜의 값 메모리가 확실히 바뀐 건지 확인하기 위함
+  // })
+  // test('정규식 복사', () => {
+  //   const reg = { reg: new RegExp(/[^0-9]/) }
+  //   const copied = deepCopyMainFnc(reg)
+  //   copied.reg = new RegExp(/[^a-z]/)
+  //   expect(copied.reg).not.toEqual(reg.reg)
+  // })
   test('Map 복사', () => {
-    const map = { map: new Map([['name', '이방원']]) }
+    const map = new Map([['name', '이방원']])
     const copied = deepCopyMainFnc(map)
-    copied.map.set('like', 'jejus')
-
-    expect(copied.map).not.toEqual(map)
+    copied.set('22', 3)
+    console.log('original', map, 'cccopied', copied)
+    // copied.set('like', 'jejus')
+    // copied.map.set('like', 'jejus')
+    expect(copied).not.toEqual(map)
+  })
+  test('Set 복사', () => {
+    const arr = ['set', '이방원']
+    const setValue = new Set(arr)
+    console.log('1111', setValue)
+    const copied = deepCopyMainFnc(setValue)
+    copied.add('set추가')
+    console.log('')
+    console.log('첫번쩨', setValue, '<====> 두번째', copied)
+    expect(copied).not.toEqual(setValue)
+  })
+  test('Array 복사', () => {
+    const arr = ['set', '이방원']
+    // console.log('1111', setValue)
+    const copied = deepCopyMainFnc(arr)
+    copied.push('fefefe')
+    console.log('')
+    console.log('첫번쩨', arr, '<====> 두번째', copied)
+    expect(copied).not.toEqual(arr)
   })
 })
