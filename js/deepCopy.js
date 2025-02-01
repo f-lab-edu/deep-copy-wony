3e808eb1fbc6657cc8eeeb83de9f4e1a2e4cd9aa
BANGWON LEE <qkddnjs31@gmail.com>
2025-02-01 18:29:11 +0900

    edit : 참조타입 객체 관련 코드 수정

--- js/deepCopy.js
+++ js/deepCopy.js
@@ -1,37 +1,17 @@
 function getNotObjConditon(obj) {
-  const notObjCondition =
-    // Array.isArray(obj) ||
-    obj === null ||
-    // obj instanceof Date ||
-    // obj instanceof RegExp ||
-    // obj instanceof Map ||
-    // obj instanceof Set ||
-    // obj instanceof WeakMap ||
-    // obj instanceof WeakSet ||
-    // typeof obj === 'symbol' ||
-    typeof obj !== 'object'
+  const notObjCondition = obj === null || typeof obj !== 'object'
 
   return notObjCondition
 }
 
-// function determineArrType(obj) {
-//   const arrType = Array.isArray(obj) || obj instanceof Set
-//   return arrType
-// }
-
 function getObjectKey(obj) {
-  console.log('3333', obj)
-  // const target = determineArrType(obj) ? [...obj] : obj
   const objArr = Object.entries(obj)
   return objArr
 }
 
-function makeUpObj(objKeyArr, obj) {
+function makeUpObj(objKeyArr) {
   const result = {}
-  console.log('objKeARR', objKeyArr)
-
   objKeyArr.forEach(([key, value], index) => {
-    console.log('objE', key, value)
     result[key] = deepCopyMainFnc(value)
   })
 
@@ -39,41 +19,47 @@ function makeUpObj(objKeyArr, obj) {
 }
 
 function deepCopyConstructor(obj) {
-  const result = new obj.constructor([...obj])
+  const result = new obj.constructor(obj)
   return result
 }
 
-function deepCopySet(obj) {
-  const result = new Set([...obj])
-  return result
+function getConstructorDecision(obj) {
+  const constructorDeciision =
+    Array.isArray(obj) ||
+    obj instanceof Date ||
+    obj instanceof RegExp ||
+    obj instanceof Map ||
+    obj instanceof Set ||
+    obj instanceof WeakMap ||
+    obj instanceof WeakSet
+
+  return constructorDeciision
 }
 
-function deepCopyMap(obj) {
-  console.log('map', obj)
-  const result = new Map(obj)
-  return result
+function determineObjFnc(obj) {
+  const constructorStaus = getConstructorDecision(obj)
+  if (constructorStaus === true) {
+    return deepCopyConstructor(obj)
+  } else {
+    const objKeysArr = getObjectKey(obj)
+    return makeUpObj(objKeysArr)
+  }
 }
-
-// function
 
 export function deepCopyMainFnc(obj) {
   const objTypeStatus = getNotObjConditon(obj)
   if (objTypeStatus) return obj
-  function determineObjFnc() {
-    if (Array.isArray(obj)) {
-      return deepCopyConstructor(obj)
-    } else if (obj instanceof Set) {
-      return deepCopyConstructor(obj)
-    } else if (obj instanceof Map) {
-      return deepCopyConstructor(obj)
-    } else {
-      const objKeysArr = getObjectKey(obj)
-      return makeUpObj(objKeysArr)
-    }
-  }
 
   return determineObjFnc(obj)
-
-  // const objKeyArr = getObjectKey(obj)
-  // return makeUpObj(objKeyArr, obj)
 }
+
+// function deepCopySet(obj) {
+//   const result = new Set([...obj])
+//   return result
+// }
+
+// function deepCopyMap(obj) {
+//   console.log('map', obj)
+//   const result = new Map(obj)
+//   return result
+// }
