2492bf02061c3ad575914bc74c7b96f211968b3e
BANGWON LEE <qkddnjs31@gmail.com>
2025-02-01 18:29:25 +0900

    edit : 테스트 코드 추가

--- test/index.test.js
+++ test/index.test.js
@@ -4,15 +4,12 @@ import { deepCopyMainFnc } from '../js/deepCopy'
 // toEqual는 객체와 같은 참조 타입
 
 describe('deepcopy check test', () => {
-  // test('원시 타입 복사', () => {
-  //   const num = 123
-  //   const copied = deepCopyMainFnc(num)
-  //   expect(num).toBe(copied)
-  // })
-  // test('null값 check', () => {
-  //   const nullValue = null
-  //   const copied = deepCopyMainFnc(nullValue)
-  // })
+  test('원시 타입 복사', () => {
+    const num = 123
+    const copied = deepCopyMainFnc(num)
+    expect(num).toBe(copied)
+  })
+
   test('객체 복사', () => {
     const obj = { num: 123, obj: { objNum: 456, str: '가나다' } }
     const copied = deepCopyMainFnc(obj)
@@ -22,19 +19,21 @@ describe('deepcopy check test', () => {
     expect(copied.obj.objNum).toBe(obj.obj.objNum)
     expect(copied.obj.str).not.toBe(obj.obj.str)
   })
-  // test('객체 안의 Date 복사', () => {
-  //   const date = { date: new Date() }
-  //   const copied = deepCopyMainFnc(date)
-  //   const editDate = new Date(copied.date).getFullYear(2030)
-  //   expect(editDate).not.toEqual(date)
-  //   // 수정한 날짜의 값 메모리가 확실히 바뀐 건지 확인하기 위함
-  // })
-  // test('정규식 복사', () => {
-  //   const reg = { reg: new RegExp(/[^0-9]/) }
-  //   const copied = deepCopyMainFnc(reg)
-  //   copied.reg = new RegExp(/[^a-z]/)
-  //   expect(copied.reg).not.toEqual(reg.reg)
-  // })
+  test('객체 안의 Date 복사', () => {
+    const date = new Date()
+    console.log('ddddeaaa', date)
+    const copied = deepCopyMainFnc(date)
+    copied.setFullYear(2030)
+    console.log('date ex', date, '<====>', copied)
+    expect(copied).not.toEqual(date)
+    // 수정한 날짜의 값 메모리가 확실히 바뀐 건지 확인하기 위함
+  })
+  test('정규식 복사', () => {
+    const reg = { reg: new RegExp(/[^0-9]/) }
+    const copied = deepCopyMainFnc(reg)
+    copied.reg = new RegExp(/[^a-z]/)
+    expect(copied.reg).not.toEqual(reg.reg)
+  })
   test('Map 복사', () => {
     const map = new Map([['name', '이방원']])
     const copied = deepCopyMainFnc(map)
