242abe91cb8f49397b1f422a64ee0f0ed899f3e6
BANGWON LEE <qkddnjs31@gmail.com>
2025-02-04 00:16:20 +0900

    edit : 일관성 있는 코드로 수정

--- js/deepCopy.js
+++ js/deepCopy.js
@@ -1,65 +1,47 @@
-const checkTypeArr = obj => Array.isArray(obj)
+const copyTypeArr = target => Array.isArray(target) && new Array(...target)
 
-const checkTypeDate = obj => obj instanceof Date
+const copyTypeDate = target => target instanceof Date && new Date(target)
 
-const checkTypeRegExp = obj => obj instanceof RegExp
+const copyTypeRegExp = target => target instanceof RegExp && new RegExp(target)
 
-const checkTypeMap = obj => obj instanceof Map
+const copyTypeMap = target => target instanceof Map && new Map(target)
 
-const checkTypeSet = obj => obj instanceof Set
+const copyTypeSet = target => target instanceof Set && new Set(target)
 
-function getNotObjConditon(obj) {
-  const notObjCondition = obj === null || typeof obj !== 'object'
+const copyJson = target => typeof target === 'object' && copyJsonObj(target)
+
+function checkTypeNotObj(target) {
+  const notObjCondition = target === null || typeof target !== 'object'
   return notObjCondition
 }
 
-function getObject(obj) {
-  const objArr = Object.entries(obj)
-  return objArr
+function getTargetArr(target) {
+  const targetArr = Object.entries(target)
+  return targetArr
 }
 
-function makeUpObj(objArr) {
+function copyJsonObj(target) {
+  const targetArr = getTargetArr(target)
   const result = {}
-  objArr.forEach(([key, value]) => {
+  targetArr.forEach(([key, value]) => {
     result[key] = deepCopyMainFnc(value)
   })
 
   return result
 }
 
-function getConstructorDecision(obj) {
-  const constructorDeciision =
-    checkTypeArr(obj) ||
-    checkTypeDate(obj) ||
-    checkTypeMap(obj) ||
-    checkTypeRegExp(obj) ||
-    checkTypeSet(obj)
-
-  return constructorDeciision
+function copyTarget(target) {
+  if (copyTypeArr(target)) return copyTypeArr(target)
+  if (copyTypeDate(target)) return copyTypeDate(target)
+  if (copyTypeRegExp(target)) return copyTypeRegExp(target)
+  if (copyTypeMap(target)) return copyTypeMap(target)
+  if (copyTypeSet(target)) return copyTypeSet(target)
+  if (copyJson(target)) return copyJson(target)
 }
 
-function CopyConstructor(obj) {
-  const result = checkTypeArr(obj)
-    ? new obj.constructor(...obj) //obj타입이 Array일 경우 스프레드 연산자를 사용함. 사용하지 않을 경우 반환될 때 이차원배열로 반환됨.
-    : new obj.constructor(obj) // obj 타입의 새로운 객체를 생성하는 방법.
+export function deepCopyMainFnc(target) {
+  const objTypeStatus = checkTypeNotObj(target)
+  if (objTypeStatus) return target
 
-  return result
-}
-
-function determineObjFnc(obj) {
-  const constructorStaus = getConstructorDecision(obj)
-
-  if (constructorStaus) {
-    return CopyConstructor(obj)
-  } else {
-    const objKeysArr = getObject(obj)
-    return makeUpObj(objKeysArr)
-  }
-}
-
-export function deepCopyMainFnc(obj) {
-  const objTypeStatus = getNotObjConditon(obj)
-  if (objTypeStatus) return obj
-
-  return determineObjFnc(obj)
+  return copyTarget(target)
 }
